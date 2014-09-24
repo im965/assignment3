2b44e7652c2c93f940d85f56fbd26de7da643ff6
Israel Malkin <israelmalkin@nyu.edu>
2014-09-24 17:47:07 -0400

    Merged in my local file for assignment 3

---
+++ im965/my_package/hello_world.py
@@ -0,0 +1,14 @@
+'''
+Created on Sep 24, 2014
+
+@author: ds-ga-1007
+
+def helloworld():
+    return "Hello World!"
+
+'''
+
+if __name__ == '__main__':
+    import sys
+    print helloworld()
+    
